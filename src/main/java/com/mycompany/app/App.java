57515fad827f6ad7393f1d13cdb55f31d52acc21
ruanjinjian <ruanjinjian@gw.com.cn>
2023-02-07 19:23:51 +0800

    second commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(MESSAGE);
         System.out.println("1");
+        System.out.println("2");
     }
 
     public String getMessage() {
