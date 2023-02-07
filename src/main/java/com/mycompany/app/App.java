c8d12c7d6240e210aa3050de69e2608a9a0240af
ruanjinjian <ruanjinjian@gw.com.cn>
2023-02-07 19:12:07 +0800

    commit six

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
-        System.out.println("123");
     }
 
     public String getMessage() {
