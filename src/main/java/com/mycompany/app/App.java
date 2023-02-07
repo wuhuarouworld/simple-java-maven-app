6d997d9d113a7bef58888f7faf99141d2eeba9a7
ruanjinjian <ruanjinjian@gw.com.cn>
2023-02-07 19:24:19 +0800

    all commit
    second commit
    
    first commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,6 +11,9 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
     }
 
     public String getMessage() {
