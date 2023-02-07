37263b4c53914f712e5a5d5380b09628874ecca2
ruanjinjian <ruanjinjian@gw.com.cn>
2023-02-07 19:24:19 +0800

    third commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -13,6 +13,7 @@ public class App {
         System.out.println(MESSAGE);
         System.out.println("1");
         System.out.println("2");
+        System.out.println("3");
     }
 
     public String getMessage() {
